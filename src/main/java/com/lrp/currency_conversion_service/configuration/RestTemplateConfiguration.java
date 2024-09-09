6a3c5834bed55c024e4deec75577ae7b7aaf2153
Lea Penarubia <princesslaion@gmail.com>
2024-09-09 21:52:05 +1000

    Added some comments for Configuration proxyBeanMethods

--- src/main/java/com/lrp/currency_conversion_service/configuration/RestTemplateConfiguration.java
+++ src/main/java/com/lrp/currency_conversion_service/configuration/RestTemplateConfiguration.java
@@ -5,10 +5,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-//TODO, if I won't put it sa Configuration, what is the diff
-//other advantage of using this approach too? aside from micrometer only?
-// //What does proxyBeanMethods mean?
+/**
+ *
+ proxyBeanMethods
 
+ true (default): Ensures SINGLETON beans by intercepting method calls, suitable for interdependent beans.
+ Usecases:
+ When your @Bean methods depend on each other and you need to ensure singleton semantics.
+
+ When you have inter-bean dependencies within the same configuration class that require method interception to maintain consistency.
+
+ --------------------------------------------------------------------------
+ false: Disables proxying for better performance when beans are independent.
+ means it will execute method directly, potentially creating new instances.
+ Calls are not intercepted.
+
+ Usecases:
+ When your @Bean methods are independent and do not call each other.
+ When aiming for improved performance and reduced memory footprint in your application.
+    coz it reduces the overhead associated with proxy creation and method interception.
+ In scenarios where configuration classes are purely for bean definitions without interdependencies.
+ */
 @Configuration (proxyBeanMethods = false)
 public class RestTemplateConfiguration {
 
